4c8d1941b7bdc347927e4eb8835adf504483def7
NewDelion <el.emena.crea@gmail.com>
2016-06-09 15:18:59 +0900

    Lobiのグループストリームを受信するプログラム

---
+++ LobiStream/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net;
+using System.Net.Sockets;
+using System.IO;
+
+
+namespace LobiStream
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            WebClient client = new WebClient();
+            client.Encoding = Encoding.UTF8;
+            using (StreamReader stream = new StreamReader(client.OpenRead("https://stream.lobi.co/1/guest/group/a5babead5099b687d25354a8c94ca81309b290e3")))
+            {
+                while (true)
+                {
+                    Console.WriteLine(stream.ReadLine());
+                }
+            }
+        }
+    }
+}
